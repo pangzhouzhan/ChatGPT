e7ccea9fba3fcc4121988dcb8e7ddee9647ba7d6
Pzz0912 <pzz@onyx-international.com>
2023-04-12 20:04:07 +0800

    update

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -41,7 +41,7 @@ onMounted(() => {
 <template>
   <NSpin :show="loading">
     <div class="p-4 space-y-4">
-			<h2 class="text-xl font-bold">
+      <h2 class="text-xl font-bold">
         Version - {{ pkg.version }}
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
@@ -55,9 +55,6 @@ onMounted(() => {
             Github
           </a>
           ，免费且基于 MIT 协议，没有任何形式的付费行为！
-        </p>
-        <p>
-          如果你觉得此项目对你有帮助，请在 Github 帮我点个 Star 或者给予一点赞助，谢谢！
         </p>
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
